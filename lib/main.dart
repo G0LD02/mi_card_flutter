5ea2e3e3847b41bb89e4f85a0cef4cb69eecda2a
Sam Alwan <samyaseralwan@gmail.com>
2026-05-09 11:14:38 +0300

    Begining of the project

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+      Myapp()
+  );
+}
+
+class Myapp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        body: Container(),
+      ),
+    );
+  }
+}
